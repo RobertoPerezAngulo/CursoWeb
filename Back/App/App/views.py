e769e04073d1a10c8edad2069007d2f45cce70dc
Jose Roberto Perez Angulo <jperez@g-ventura.com>
2024-02-01 19:10:52 -0600

    Intgreso back django rest

---
+++ Back/App/App/views.py
@@ -0,0 +1,13 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import requests
+
+class PokemonsViewers(APIView):
+    def get(self, request, *args, **kwargs):
+        response = requests.get("https://pokeapi.co/api/v2/pokemon/?offset=1&limit=20")
+        data = response.json()
+        collections = []
+        offset = 1
+        for index, item in enumerate(data.get('results'), start=offset):
+            collections.append({"name": item.get("name"),"img": f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{index}.png"})
+        return Response(collections)
